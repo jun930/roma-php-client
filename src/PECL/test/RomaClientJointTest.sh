d2a532a451111810d0c725fd02226d1d02bccab9
Jun Takizawa <fire@jun-t.com>
2011-04-08 14:54:02 +0900

    add tests

---
+++ src/PECL/test/RomaClientJointTest.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+echo ***TEST*** RomaClientJointTest0
+phpunit RomaClientJointTest0.php
+echo ***TEST*** RomaClientJointTest1
+phpunit RomaClientJointTest1.php
+echo ***TEST*** RomaClientJointTest2
+phpunit RomaClientJointTest2.php
+echo ***TEST*** RomaClientJointTest3
+phpunit RomaClientJointTest3.php
+echo ***TEST*** RomaClientJointTest4
+phpunit RomaClientJointTest4.php
+echo ***TEST*** RomaClientJointTest5
+phpunit RomaClientJointTest5.php
+echo ***TEST*** RomaClientJointTest6
+phpunit RomaClientJointTest6.php
+echo ***TEST*** RomaClientJointTest7
+phpunit RomaClientJointTest7.php
+echo ***TEST*** RomaClientJointTest8
+phpunit RomaClientJointTest8.php
+echo ***TEST*** RomaClientJointTest9
+phpunit RomaClientJointTest9.php
+echo ***TEST*** RomaClientJointTest10
+phpunit RomaClientJointTest10.php
+echo ***TEST*** RomaClientJointTest11
+phpunit RomaClientJointTest11.php
+echo ***TEST*** RomaClientJointTest12
+phpunit RomaClientJointTest12.php
+echo ***TEST*** RomaClientJointTest13
+phpunit RomaClientJointTest13.php
+echo ***TEST*** RomaClientJointTest14
+phpunit RomaClientJointTest14.php
+echo ***TEST*** RomaClientJointTest15
+phpunit RomaClientJointTest15.php
+echo ***TEST*** RomaClientJointTest16
+phpunit RomaClientJointTest16.php
+echo ***TEST*** RomaClientJointTest17
+phpunit RomaClientJointTest17.php
+echo ***TEST*** RomaClientJointTest18
+phpunit RomaClientJointTest18.php
+echo ***TEST*** RomaClientJointTest19
+phpunit RomaClientJointTest19.php
+echo ***TEST*** RomaClientJointTest20
+phpunit RomaClientJointTest20.php
+
