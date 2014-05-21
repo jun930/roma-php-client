ef1167ed9ac379ea6d0e10a99165ebccebe6d15c
firejun <fire@jun-t.com>
2014-05-21 17:18:08 +0900

    add for 64bit version and modify for PHPUnit

--- src/PECL/build.sh
+++ src/PECL/build.sh
@@ -2,11 +2,17 @@
 cd `dirname $0`
 export LD_LIBRARY_PATH=`pwd`/../lib:$LD_LIBRARY_PATH
 export TZ=Asia/Tokyo 
-pecl-gen -f rmcc.xml
+if [ "$M64" == "1" ]; then
+    pecl-gen -f rmcc64.xml
+else
+    pecl-gen -f rmcc.xml
+fi
 cd phprmcc
 phpize
 if [ "$WITH_CONFIGURE" == "1" ];then
     ./configure CPPFLAGS="-I`pwd`/../../include"
+    mv Makefile Makefile.org
+    sed -e "s/\$(PHP_EXECUTABLE) -n -c \$(top_builddir)\/tmp-php\.ini \$(PHP_TEST_SETTINGS) \$(top_srcdir)\/run-tests\.php -n -c \$(top_builddir)\/tmp-php\.ini -d extension_dir=\$(top_builddir)\/modules\/ \$(PHP_TEST_SHARED_EXTENSIONS) \$(TESTS); \\\/\$(PHP_EXECUTABLE) -n \$(PHP_TEST_SETTINGS) \$(top_srcdir)\/run-tests\.php -n -c \$(top_builddir)\/tmp-php\.ini -d extension_dir=\$(top_builddir)\/modules\/ \$(PHP_TEST_SHARED_EXTENSIONS) \$(TESTS); \\\/" Makefile.org > Makefile
 fi
 make clean all test LDFLAGS=-L../../lib
 cd ..
@@ -14,10 +20,10 @@ if [ "$WITH_TEST" == "1" ];then
     `which phpunit` --coverage-html xdebug PhpunitEnv
     source php.env
     cp -f $PHP_EXT_DIR/* `pwd`/phprmcc/modules/
-    echo "extension_dir=`pwd`/phprmcc/modules" >> php.ini 
+    echo "extension_dir=`pwd`/phprmcc/modules" > php.ini 
     #echo "extension_dir=/usr/lib/php/modules"
     echo "extension=phprmcc.so" >> php.ini 
-    echo "extension=xdebug.so" >> php.ini 
+    echo "zend_extension=`pwd`/xdebug/xdebug.so" >> php.ini 
     php -c php.ini `which phpunit` --coverage-html xdebug test/AllTest
 fi
 # sudo make install
